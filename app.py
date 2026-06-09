56a1d8c5adbbc80493c903a3e432e879fa859def
febinsiju <febinsijuarackan@gmail.com>
2026-06-09 18:19:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,3 +40,25 @@ joblib.dump(model, "model.pkl")
 joblib.dump(vectorizer, "vectorizer.pkl")
 
 print("Model and vectorizer saved successfully!")
+from flask import Flask, render_template, request
+import joblib
+
+app = Flask(__name__)
+
+model = joblib.load("model.pkl")
+vectorizer = joblib.load("vectorizer.pkl")
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    result = None
+
+    if request.method == "POST":
+        news_text = request.form["news"]
+        news_vector = vectorizer.transform([news_text])
+        prediction = model.predict(news_vector)[0]
+        result = prediction
+
+    return render_template("index.html", result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
